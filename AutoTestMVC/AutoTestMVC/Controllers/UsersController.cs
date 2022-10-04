9ad77fca97a19662fc85cef8955e4e2028901dc1
Ogabek <xolmirzayev.ogabek.2004@gmail.com>
2022-10-04 15:40:10 +0500

    lorem ipsum

--- AutoTestMVC/AutoTestMVC/Controllers/UsersController.cs
+++ AutoTestMVC/AutoTestMVC/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoTestMVC.Models;
 using AutoTestMVC.Repositories;
+using AutoTestMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AutoTestMVC.Controllers
@@ -7,15 +8,32 @@ namespace AutoTestMVC.Controllers
     public class UsersController : Controller
     {
         private readonly UsersRepository _usersRepository;
+        private readonly CookiesService _cookiesService;
 
         public UsersController()
         {
             _usersRepository = new UsersRepository();
+            _cookiesService = new CookiesService();
         }
 
         public IActionResult Index()
         {
+            var userPhone = _cookiesService.GetUserPhoneFromCookie(HttpContext);
 
+            if (userPhone != null)
+            {
+                var user = _usersRepository.GetUserByPhone(userPhone);
+                if (user.Phone == userPhone)
+                {
+                    return View(user);
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
             return View();
         }
 
@@ -27,7 +45,20 @@ namespace AutoTestMVC.Controllers
         public IActionResult SignupPost(User user)
         {
             _usersRepository.InsertUser(user);
+            _cookiesService.SendUserPhoneToCookie(user.Phone!, HttpContext);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult SigninPost(User user)
+        {
+            var _user = _usersRepository.GetUserByPhone(user.Phone!);
+
+            if (user.Password == _user.Password)
+            {
+                _cookiesService.SendUserPhoneToCookie(user.Phone!, HttpContext);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Signin");
         }
     }
 }
---
+++ AutoTestMVC/AutoTestMVC/Services/CookiesService.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoTestMVC.Services
+{
+    public class CookiesService
+    {
+        public string? GetUserPhoneFromCookie(HttpContext context)
+        {
+            if (context.Request.Cookies.ContainsKey("UserPhone"))
+                return context.Request.Cookies["UserPhone"];
+
+            return null;
+        }
+
+        public void SendUserPhoneToCookie(string userPhone, HttpContext context)
+        {
+            context.Response.Cookies.Append("UserPhone", userPhone);
+        }
+    }
+}
